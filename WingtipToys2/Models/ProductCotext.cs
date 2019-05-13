71ba3936d31bbd9b1e89f8639b18eff31a89f69d
Alex <achphoenix@gmai.com>
2019-05-14 11:03:48 +1200

    Add project files.

---
+++ WingtipToys2/Models/ProductCotext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WingtipToys2.Models
+{
+    public class ProductCotext: DbContext
+    {
+        public ProductCotext() : base("Wingtiptoys2") { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products{ get; set; }
+
+    }
+}
